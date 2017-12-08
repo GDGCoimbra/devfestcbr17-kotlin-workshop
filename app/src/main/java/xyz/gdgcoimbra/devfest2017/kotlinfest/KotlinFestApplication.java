6b181181d6aeabdef84acf06515db061d20c7c2d
Renato Almeida <renato.m.alm@gmail.com>
2017-12-08 16:39:41 +0000

    Project init 🚀

---
+++ app/src/main/java/xyz/gdgcoimbra/devfest2017/kotlinfest/KotlinFestApplication.java
@@ -0,0 +1,11 @@
+package xyz.gdgcoimbra.devfest2017.kotlinfest;
+
+import android.app.Application;
+
+public class KotlinFestApplication extends Application {
+
+    @Override
+    public void onCreate() {
+        super.onCreate();
+    }
+}
